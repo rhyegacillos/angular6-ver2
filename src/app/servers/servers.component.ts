69925043e87d28f8a8e20c98c290719dc817bedd
Rhye Gacillos <gacillos.rhye@gmail.com>
2018-09-22 19:41:22 +0800

    - basic angular completed.

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,13 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
  allowNewServer = false;
-  constructor() {
+ serverCreationStatus = 'No server was created';
+ serverName: string;
+ serverCreated = false;
+ servers = ['Testserver', 'Testserver 2'];
+ constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
     }, 2000);
   }
 
   ngOnInit() {
+  }
+
+  onCreateServer() {
+   this.serverCreated = true;
+   this.servers.push(this.serverName);
+   this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: any) {
+   this.serverName = event.target.value;
   }
 
 }
