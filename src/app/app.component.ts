52e9a98c7b8c7e2144c8393d9d8677f8e5b55d6d
Rhye Gacillos <gacillos.rhye@gmail.com>
2018-10-20 21:14:47 +0800

    - forms - template driven - assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  @ViewChild('f') myForm: NgForm;
+  subscriptions = ['Basic', 'Advance', 'Pro'];
+  submitted = false;
+  signUpForm = {
+    email: '',
+    subscription: '',
+    password: ''
+  };
+
+  onSubmit() {
+    this.submitted = true;
+    console.log(this.signUpForm);
+    this.signUpForm.email = this.myForm.form.value.email;
+    this.signUpForm.subscription = this.myForm.form.value.subscription;
+    this.signUpForm.password = this.myForm.form.value.password;
+
+    this.myForm.reset();
+  }
 }
