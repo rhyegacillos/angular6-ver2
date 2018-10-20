a85bccd0643274ea304070f93d34bd2474cc02c6
Rhye Gacillos <gacillos.rhye@gmail.com>
2018-10-20 17:40:46 +0800

    - forms - template driven

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,34 @@ import {NgForm} from '@angular/forms';
 })
 export class AppComponent {
 
-  @ViewChild('f') signupForm = NgForm
-  // onSubmit(form: NgForm) {
-  //   console.log(form);
-  // }
+  @ViewChild('f') signupForm: NgForm;
+  answer: string;
+  submitted = false;
+  genders = ['Male', 'Female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+    this.signupForm.form.patchValue({userData: {
+      username: suggestedName
+      }});
+  }
 
   onSubmit() {
-    console.log(this.signupForm);
+    this.submitted = true;
+    this.user.username = this.signupForm.form.value.userData.username;
+    this.user.email = this.signupForm.form.value.userData.email;
+    this.user.secretQuestion = this.signupForm.form.value.secret;
+    this.user.answer = this.signupForm.form.value.questionAnswer;
+    this.user.gender = this.signupForm.form.value.gender;
+
+    this.signupForm.reset();
+
   }
 }
